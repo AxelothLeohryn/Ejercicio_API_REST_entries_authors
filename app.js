901d433dfa903556daf9964e7c3c7d6c241923ca
AxelothLeohryn <alejandro97ss@gmail.com>
2023-11-14 09:41:37 +0100

    add error404

--- app.js
+++ app.js
@@ -3,8 +3,10 @@ const morgan = require("./middlewares/morgan.js");
 const app = express();
 const port = 3000;
 app.use(express.json());
+const error404 = require("./middlewares/error404");
 // Logger
 app.use(morgan(":method :host :status :param[id] - :response-time ms :body"));
+
 
 const entriesRouter = require("./routes/entries.routes.js");
 const authorsRouter = require("./routes/authors.routes.js");
@@ -13,6 +15,9 @@ const authorsRouter = require("./routes/authors.routes.js");
 app.use("/api/entries", entriesRouter.router);
 app.use("/api/authors", authorsRouter.router);
 
+
+//Para rutas no existentes
+app.use("*", error404);
 app.listen(port, function () {
   console.log(`Example app listening on port ${port}!`);
 });
